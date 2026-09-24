960a60694f7cd645c8aa3d3b0476be841accffc7
Ansh Awasthi <anshawasthii2727@gmail.com>
2026-09-24 19:02:59 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,64 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
 
 const App = () => {
 
-  const [title, setTitle] = useState()
-  const [details, setDetails] = useState()
+  const [userData, setUserData] = useState([])
+  const [index, setIndex] = useState(1)
 
-  const [task, setTask] = useState([])
-
-  const SubmitHandler = (e) => {   
-    e.preventDefault()
-    const copyTask = [...task];
-    copyTask.push({title, details})
-    setTask(copyTask)
-
-    setTitle('')
-    setDetails('')
+  const getData = async () => {
+    const response = await axios.get(`https://picsum.photos/v2/list?page=${index}&limit=10`)
+    setUserData(response.data);
   }
 
-  const deleteNote = (idx) => {
-    const copyTask = [...task]
-    copyTask.splice(idx, 1)
-    setTask(copyTask)    
+  useEffect(function(){
+    getData()
+  }, [index])
+
+  let printUserData = <h3 className='text-gray-400 text-xs absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 font-semibold'>Loading...</h3>
+
+  if(userData.length > 0){
+    printUserData = userData.map(function(elem, idx){
+      return <div key={idx}>
+          <a href={elem.url}>
+
+            <div className='h-40 w-44 overflow-hidden rounded-xl bg-white'>
+              <img className='h-full object-cover' src={elem.download_url} alt="" />
+
+            </div>
+            <h2>{elem.author}</h2>
+          </a>
+        </div>
+    })
   }
 
   return (
-    <div className='h-screen lg:flex bg-black text-white'>
+    <div className='h-screen w-screen overflow-auto bg-black text-white p-4'>
 
-      <form onSubmit={(e)=>{
-        SubmitHandler(e);        
-      }}
-       className='flex p-10 gap-4 lg:w-1/2 items-start flex-col'>
-        <h1 className='text-4xl font-bold'>Add Notes</h1>
-        <input type="text" placeholder='Enter Title' className='border-2 font-medium w-full px-5 py-2 outline-none rounded' value={title} onChange={(e)=>{
-          setTitle(e.target.value)
-        }}/>
-        <textarea placeholder='Write details' name="" id="" className='border-2 font-medium w-full flex items-start px-5 py-5 h-32 flex-row outline-none rounded' value={details} onChange={(e)=>{
-          setDetails(e.target.value)
-        }}></textarea>
-        <button className='bg-amber-50 font-medium active:bg-white active:scale-95 cursor-pointer w-full text-black px-5 py-1 outline-none'>Add Note</button>
-      </form>
-
-      <div className='p-10 lg:w-1/2 lg:border-l-2'>
-        <h1 className='text-4xl font-bold'>Recent Notes</h1>
-        <div className='flex flex-wrap items-start justify-start mt-6 gap-5 h-[90%] overflow-auto'>
-          {task.map(function(elem, idx) {
-            return <div key={idx} className="flex flex-col justify-between items-start relative bg-[url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnLPnW7YWMbLsXu_OPx0AzE4A5fDNXEEo_Bf7ynff9VA&s=10')] bg-cover rounded-2xl text-black py-9 pb-4 px-4 h-52 w-40">
-              <h3 className='leading-tight text-lg font-bold'>{elem.title}</h3>
-              <p className='mt-4 leading-tight text-xs font-semibold text-gray-700'>{elem.details}</p>
-              <button onClick={() => {
-                deleteNote(idx)
-              }} className='bg-red-800 text-white cursor-pointer active:scale-95 active:bg-red-900 py-1 rounded text-xs font-bold w-full'>Delete</button>
-            </div>
-          })}          
-        </div>
+      <div className='flex h-[82%] flex-wrap gap-4 p-2'>
+        {printUserData}
       </div>
 
+      <div className='p-4 flex justify-center gap-6 bg-black'>
+        <button 
+        style={{opacity : index == 1 ? 0.5 : 1}}
+        onClick={()=>{
+          if(index > 1){
+            setIndex(index-1)
+            setUserData([])
+          }
+        }}
+        className='bg-amber-400 text-black text-sm rounded px-4 py-1 cursor-pointer active:scale-95 font-semibold'>Prev</button>
+
+        <h3 className='font-semibold'>Page {index}</h3>
+
+        <button 
+        onClick={()=>{
+          setUserData([])
+          setIndex(index+1)
+        }} 
+        className='bg-amber-400 text-black text-sm rounded px-4 py-1 cursor-pointer active:scale-95 font-semibold'>Next</button>
+      </div>
     </div>
   )
 }
